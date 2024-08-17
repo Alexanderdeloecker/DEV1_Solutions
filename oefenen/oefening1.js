facc20c205d87eed9c0680189cdb49e2468ac501
Alexander De Loecker <Alexander.De.Loecker@student.ehb.be>
2024-08-17 13:47:12 +0200

    her

--- oefenen/oefening1.js
+++ oefenen/oefening1.js
@@ -1,11 +1,25 @@
 "use strict";
 
+import context from "../../scripts/context.js";
+import * as Utils from "../../scripts/utils.js";
+
 let canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-let context = canvas.getContext('2d');
 
-drawName();
+let width = context.canvas.width;
+let height = context.canvas.height;
+let x = 400
+let y = 300
+let radius = 50
+let speed = 5;
+let goRight = true;
+
+//drawPatr();
+//drawName();
+
+//drawCircle()
+
 
 function drawName() {
 
@@ -40,4 +54,62 @@ function drawName() {
     context.stroke()
    
 
+}
+
+function drawPatr(){
+
+    for (let i = 1 ; i <= 21 ; i++){
+        for (let j = 1 ; j <= 10 ; j++){
+            context.fillStyle = "grey"
+            context.fillRect(-50+i*60,-50+j*60,53,53)
+        }
+    }
+}
+
+function drawBackground(){
+
+    context.fillStyle = "blue"
+    context.fillRect(0,0,width/2,height/2)
+    context.fillStyle = "green"
+    context.fillRect(width/2,0,width/2,height/2)
+    context.fillStyle = "green"
+    context.fillRect(0,height/2,width/2,height/2)
+    context.fillStyle = "blue"
+    context.fillRect(width/2,height/2,width/2,height/2)
+}
+
+function drawCircle(){
+    drawBackground()
+    context.fillStyle= "red"
+    Utils.fillCircle(x, y, radius);
+    if (x >= width - radius) {
+        goRight = false;
+    } else if (x <= radius) {
+        goRight = true;
+    }
+
+    if (goRight == true) {
+        x += speed;
+    } else {
+        x -= speed;
+    }
+
+
+    context.fillStyle= "blue"
+    Utils.fillCircle(x, y, radius);
+    if (x >= width - radius) {
+        goRight = false;
+    } else if (x <= radius) {
+        goRight = true;
+    }
+
+    if (goRight == true) {
+        x += speed;
+    } else {
+        x -= speed;
+    }
+    
+
+    requestAnimationFrame(drawCircle);
+    console.log(drawCircle)
 }
